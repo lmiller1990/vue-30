607fb02618a3bef0987f1b47797c639bb0c1e1f1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-04-27 18:11:47 +1000

    wip

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
-<<<<<<< HEAD
 import App from './store/StoreApp.vue'
-=======
-// import App from './useShortcut/App.vue'
-import App from './light-box/App.vue'
->>>>>>> 9378d22434a933277fff6cb2c6dd7462bb9c9031
 import './index.css'
 
 const app = createApp(App)
